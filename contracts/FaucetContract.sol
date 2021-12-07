9e7a728ff8e17b525a6391b1e9bdc96577a9b539
Zeztron <pateltharsh@gmail.com>
2021-12-07 10:20:59 -0600

    created faucet and storage contracts

---
+++ contracts/FaucetContract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+
+  uint public numOfFunders;
+  mapping(address => bool) private funders;
+  mapping(uint => address) private lutFunders;
+
+  receive() external payable {}
+
+  function addFunds() external payable{
+    address funder = msg.sender;
+
+    if (!funders[funder]) {
+      uint index = numOfFunders++;
+      funders[funder] = true;
+      lutFunders[index] = funder;
+    }
+  }
+
+  function getAllFunders() external view returns (address[] memory) {
+    address[] memory _funders = new address[](numOfFunders);
+
+    for (uint i = 0; i < numOfFunders; i++) {
+      _funders[i] = lutFunders[i];
+    }
+
+    return _funders;
+  }
+
+  function getFunderAtIndex(uint index) external view returns(address) {
+    return lutFunders[index];
+  }
+}
+
+// const instance = await Faucet.deployed()
+// instance.addFunds({from: accounts[0], value: "2"})
