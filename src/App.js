8c36e9d47525b78cf0247d64ba41fb51a1df7106
Zeztron <pateltharsh@gmail.com>
2021-12-09 08:57:29 -0600

    connect metamask onclick

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
 import './App.css';
 
 const App = () => {
@@ -8,31 +9,16 @@ const App = () => {
 
   useEffect(() => {
     const loadProvider = async () => {
-      const provider =
-        window.ethereum ||
-        window.web3 ||
-        new Web3.providers.HttpProvider('http://localhost:7545');
+      const provider = await detectEthereumProvider();
 
-      if (window.ethereum) {
-        try {
-          await provider.enable();
-        } catch {
-          console.error('User denied accounts access!');
-        }
+      if (provider) {
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+        });
+      } else {
+        console.error('Please, install Metamask.');
       }
-
-      // if (window.ethereum) {
-      //   provider = window.ethereum;
-      // } else if (window.web3) {
-      //   provider = window.web3.currentProvider;
-      // } else if (!process.env.production) {
-      //   provider = new Web3.providers.HttpProvider('http://localhost:7545');
-      // }
-
-      setWeb3Api({
-        web3: new Web3(provider),
-        provider,
-      });
     };
 
     loadProvider();
@@ -47,22 +33,32 @@ const App = () => {
     web3Api.web3 && getAccount();
   }, [web3Api.web3]);
 
-  console.log(account);
+  const connectMetamask = () =>
+    web3Api.provider.request({ method: 'eth_requestAccounts' });
 
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <span>
-            <strong>Account: </strong>
-          </span>
-          <h1>{account ? account : 'Not Connected'}</h1>
-          <div className="balance-view is-size-2">
+          <div className="is-flex is-align-items-center">
+            <span>
+              <strong className="mr-2">Account: </strong>
+            </span>
+
+            {account ? (
+              <span>{account}</span>
+            ) : (
+              <button onClick={connectMetamask} className="button">
+                Connect Metamask
+              </button>
+            )}
+          </div>
+          <div className="balance-view is-size-2 my-5">
             Current Balance: <strong>10</strong> ETH
           </div>
 
-          <button className="btn mr-2">Donate</button>
-          <button className="btn">Withdraw</button>
+          <button className="button is-link mr-2">Donate</button>
+          <button className="button is-primary">Withdraw</button>
         </div>
       </div>
     </>
